32a68fb95d5c60c8fe5504a034d77436f5905bb3
gauravkumarpandey <48739979+gauravkumarpandey@users.noreply.github.com>
2020-01-12 01:54:16 +0530

    Add missing files

---
+++ restwebservice/src/main/java/com/example/restwebservice/helloword/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.restwebservice.helloword;
+
+public class HelloWorld {
+
+	private final String message;
+	public HelloWorld(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+}
